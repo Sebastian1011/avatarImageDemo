73a8206bfa29115f82addacab10cf8cbf92aa890
ziming zhang <zzmbpt@163.com>
2015-08-15 21:58:02 +0800

    UIActionSheet

--- avatarImage/avatarImage/ViewController.swift
+++ avatarImage/avatarImage/ViewController.swift
@@ -23,6 +23,48 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     
     func imageViewTouch(){
         print(22)
+        /*
+        //代理设置
+        self.imagePickerController.delegate = self
+        //设置允许管理硬盘中图片
+        self.imagePickerController.allowsEditing = true
+        //判断是否支持相机
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            let alertController:UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+            //iPad上使用表单需要设置anchor point
+            var popover = alertController.popoverPresentationController
+            /*if(popover != nil){
+                popover?.sourceView = sender
+                popover?.sourceRect = sender.bounds
+                popover?.permittedArrowDirections = UIPopoverArrowDirection.Any
+                
+            }*/
+            let cameraAction: UIAlertAction = UIAlertAction(title: "拍照", style: .Default){(action:UIAlertAction!) -> Void in
+                //设置类型
+                self.imagePickerController.sourceType = UIImagePickerControllerSourceType.Camera
+                self.presentViewController(self.imagePickerController, animated: true, completion: nil)
+            }
+            
+            alertController.addAction(cameraAction)
+            
+
+            
+        }*/
+        let alertController = UIAlertController(title: "Test", message: "just do it", preferredStyle: .ActionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {(action) in
+            print("aaaa")
+        })
+        alertController.addAction(cancelAction)
+        let cameraAction = UIAlertAction(title: "从相机", style: .Default, handler: {(action) in
+            print("bbb")
+        })
+        alertController.addAction(cameraAction)
+        let galeryAction = UIAlertAction(title: "从相册", style: .Default, handler: {(action) in
+            print("ccc")
+        })
+        alertController.addAction(galeryAction)
+       
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
