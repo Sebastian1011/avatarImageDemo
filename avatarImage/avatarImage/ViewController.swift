d1162b358efa6b05d6a2a679cd55f002516d8cbf
ziming zhang <zzmbpt@163.com>
2015-08-16 20:00:28 +0800

    actionsheet/photolibary

--- avatarImage/avatarImage/ViewController.swift
+++ avatarImage/avatarImage/ViewController.swift
@@ -22,12 +22,14 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     }
     
     func imageViewTouch(){
-        print(22)
-        /*
         //代理设置
         self.imagePickerController.delegate = self
         //设置允许管理硬盘中图片
         self.imagePickerController.allowsEditing = true
+        
+        
+        
+        /*
         //判断是否支持相机
         if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
             let alertController:UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
@@ -50,22 +52,77 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
 
             
         }*/
+        //添加alertcontroller 样式为actionsheet
         let alertController = UIAlertController(title: "Test", message: "just do it", preferredStyle: .ActionSheet)
+        var popover = alertController.popoverPresentationController
+        if (popover != nil){
+            popover?.sourceView = UIView()
+            popover?.sourceRect = UIView().bounds
+            popover?.permittedArrowDirections = UIPopoverArrowDirection.Any
+            
+        }
+        
+        //添加取消行为
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {(action) in
             print("aaaa")
         })
         alertController.addAction(cancelAction)
-        let cameraAction = UIAlertAction(title: "从相机", style: .Default, handler: {(action) in
-            print("bbb")
-        })
-        alertController.addAction(cameraAction)
+        
+        //添加从相机获取头像
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            let cameraAction = UIAlertAction(title: "从相机获取头像", style: .Default, handler: {(action) in
+                print("bbb")
+                self.imagePickerController.sourceType = UIImagePickerControllerSourceType.Camera
+                self.presentViewController(self.imagePickerController, animated: true, completion: nil)
+                
+            })
+            
+            alertController.addAction(cameraAction)
+            
+        }
+
+        
+        //添加从相册获取头像
         let galeryAction = UIAlertAction(title: "从相册", style: .Default, handler: {(action) in
             print("ccc")
+            self.imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            //更改navigationbar的背景色
+            self.imagePickerController.navigationBar.barTintColor = UIColor(red: 171/255, green: 202/255, blue: 41/255, alpha: 1.0)
+            
+            //更改navigationbar的标题色
+            self.imagePickerController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+            
+            //更改navigationbar的字体色
+            self.imagePickerController.navigationBar.tintColor = UIColor.whiteColor()
+            
+            self.presentViewController(self.imagePickerController, animated: true, completion: nil)
+            
+            
+            
         })
         alertController.addAction(galeryAction)
-       
+        
+        //显示actionsheet
         self.presentViewController(alertController, animated: true, completion: nil)
     }
+    
+    //imagePicker 事件实现
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        var image:UIImage!
+        
+        //判断是否可以修改
+        if (picker.allowsEditing){
+            //剪裁图片
+            //image = info[UIImagePickerControllerEditedImage] as! UIImage
+            
+        }
+        else{
+            //image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        }
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
